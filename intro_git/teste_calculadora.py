94cd17a0542e9b17b86c0e1d9c0cd1572ac57d72
Renan Antonio <renan.amsouza8@gmail.com>
2026-02-28 15:39:08 -0300

    test(ap01-git): testa a soma e a subtracao

---
+++ intro_git/teste_calculadora.py
@@ -0,0 +1,9 @@
+import calculadora
+
+a = 10
+b = 20
+resultado = calculadora.soma(a,b)
+print(f"Resultado da soma: {resultado}")
+
+resultado_subtracao = calculadora.subtracao(a,b)
+print(f"Resultado da subtração: {resultado_subtracao}")
