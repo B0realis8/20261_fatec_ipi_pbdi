4049e8f4f31adc814263368c128b588c7dd98f38
Renan Antonio <renan.amsouza8@gmail.com>
2026-03-17 10:37:29 -0300

    learn(ap10): adiciona um loop interno e um externo e insere nomenclaturas para ambos

--- ap10/script.sql
+++ ap10/script.sql
@@ -15,3 +15,25 @@ BEGIN
     END LOOP;
 END;
 $$
+
+DO $$
+DECLARE
+    i INT;
+    j INT;
+BEGIN
+    i:= 0;
+    <<externo>>
+    LOOP
+
+        i = i + 1;
+        EXIT WHEN i > 10;
+        j := 1;
+        <<interno>>
+        LOOP
+            RAISE NOTICE '% , %', i, j;
+            j := j+1;
+            EXIT WHEN j> 10;
+            CONTINUE externo WHEN j > 5;
+        END LOOP;
+    END LOOP;
+END $$
