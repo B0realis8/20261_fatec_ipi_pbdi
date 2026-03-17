7c4408596d02fdfa157ea318a7292286e0482107
Renan Antonio <renan.amsouza8@gmail.com>
2026-03-17 10:23:38 -0300

    learn(ap10): modifica o contador para remover alguns valores

--- ap10/script.sql
+++ ap10/script.sql
@@ -3,11 +3,15 @@ DECLARE
     contador INT = 0;
 BEGIN
     LOOP
-        RAISE NOTICE '%', contador;
         contador = contador +1; --não tem contador
-        IF contador > 10 THEN
-            EXIT;
+        EXIT WHEN contador >100;
+
+        IF contador % 7 = 0 THEN -- remove multiplos de 7
+            CONTINUE;
         END IF;
+        CONTINUE WHEN contador % 11 = 0; -- remove multiplos de 11
+        RAISE NOTICE '%', contador;
+
     END LOOP;
 END;
 $$
