3ec70ef6d1a02354955becdc3dc0ce8075ee974d
Renan Antonio <renan.amsouza8@gmail.com>
2026-03-24 10:29:19 -0300

    learn(ap16): implementa script de um cursor

--- ap16/script.sql
+++ ap16/script.sql
@@ -11,4 +11,23 @@ CREATE TABLE tb_top_youtubers(
 
 SELECT * FROM tb_top_youtubers;
 
-
+DO $$
+DECLARE
+    --declaração do CURSOR, inicialmente unbound por não estar vinculado a uma query
+    cur_nomes_youtubers REFCURSOR;
+    v_youtubers VARCHAR(200); --variável para armazenar os nomes dos youtubers
+BEGIN
+    -- abertura do cursor
+    OPEN cur_nomes_youtubers FOR
+        SELECT youtuber
+            FROM
+            tb_top_youtubers;
+    LOOP
+        -- Reuperando os dados e inserindo na variável
+        FETCH cur_nomes_youtubers INTO v_youtubers;
+        EXIT WHEN NOT FOUND;
+        RAISE NOTICE '%', v_youtubers;
+    END LOOP;
+    CLOSE cur_nomes_youtubers;
+END;
+$$
