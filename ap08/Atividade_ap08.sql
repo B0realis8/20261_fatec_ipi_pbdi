5eb61e7ea652fdeb706161e964ca85be3b465ba6
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-05 01:12:58 -0300

    learn(ap08): respostas dos exericicos de 1 a 7

--- ap08/Atividade_ap08.sql
+++ ap08/Atividade_ap08.sql
@@ -64,3 +64,72 @@ BEGIN
     RAISE NOTICE 'A raiz x2 = %',x2;
 END;
 $$
+
+--1.5 Faça umprograma que gere um número inteiro e mostre a raiz cúbica de seu antecessor
+--e a raiz quadrada de seu sucessor
+
+DO $$
+DECLARE
+    n INTEGER;
+    rc NUMERIC;
+    rq NUMERIC;
+BEGIN
+    n := floor(random()*100+1);
+    RAISE NOTICE 'Número: %',n;
+    rq := sqrt(n+1);
+    RAISE NOTICE 'Raíz quadrada do sucessor: %',rq;
+    rc := (n-1)^(1::numeric/3);
+    RAISE NOTICE 'Raíz cúbica do antecessor: %',rc;
+END;
+$$
+
+--1.6 Faça um programa que gere medidas reais de um terreno retangular. Gere também um
+--valor real no intervalo [60, 70] que representa o preço por metro quadrado. O programa deve
+--exibir o valor total do terreno.
+
+DO $$
+DECLARE
+    _a NUMERIC(5,2);
+    _b NUMERIC(5,2);
+    metro_quadrado NUMERIC(10,2);
+    preco NUMERIC(4,2);
+    p_min INTEGER := 60;
+    p_max INTEGER := 70;
+    valor NUMERIC (10,2);
+BEGIN
+    _a := (random()*100);
+    _b := (random()*100);
+    metro_quadrado := _a * _b;
+    RAISE NOTICE 'Dimensões do terreno: %m x %m',_a,_b;
+    RAISE NOTICE 'Tamanho total: %m²',metro_quadrado;
+    preco := random() * (p_max - p_min) + p_min;
+    RAISE NOTICE 'Valor do metro quadrado: R$%',preco;
+    valor := metro_quadrado*preco;
+    RAISE NOTICE 'Valor total do terreno: R$%',valor;
+END;
+$$
+
+
+--.7 Escreva um programa que gere um inteiro que representa o ano de nascimento de uma
+--pessoa no intervalo [1980, 2000] e gere um inteiro que representa o ano atual no intervalo
+--[2010, 2020]. O programa deve exibir a idade da pessoa em anos. Desconsidere detalhes
+--envolvendo dias, meses, anos bissextos etc.
+
+DO $$
+DECLARE
+    n INTEGER;
+    n_min INTEGER := 1980;
+    n_max INTEGER := 2000;
+    ano INTEGER;
+    ano_min INTEGER := 2010;
+    ano_max INTEGER := 2020;
+    idade INTEGER;
+BEGIN
+    n := random()*(n_max-n_min)+n_min;
+    RAISE NOTICE 'Ano de nascimento: %',n;
+    ano := random()*(ano_max-ano_min)+ano_min;
+    RAISE NOTICE 'Ano atual: %',ano;
+    idade := ano-n;
+    RAISE NOTICE 'Idade: % anos',idade;
+END;
+$$
